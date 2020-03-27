3b13c8d79efcd70794c761df58ed63aa2214230e
mulananda <mulanandaa@gmail.com>
2020-03-27 23:29:25 +0700

    First commit mulananda store

---
+++ src/components/CarouselBanner.vue
@@ -0,0 +1,127 @@
+<template>
+  <!-- Women Banner Section Begin -->
+  <section class="women-banner spad">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 mt-5">
+          <carousel class="product-slider" :items="3" :autoplay="true" :nav="false">
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/cloth/jaket_zara1-1.jpg" alt />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#">
+                      <i class="icon_bag_alt"></i>
+                    </a>
+                  </li>
+                  <li class="quick-view">
+                    <router-link to="/product">+ Quick View</router-link>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Jaket</div>
+                <a href="#">
+                  <h5>TWEED JACKET</h5>
+                </a>
+                <div class="product-price">
+                  $30.00
+                  <span>$35.00</span>
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/cloth/jaket_zara2-1.jpg" alt />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#">
+                      <i class="icon_bag_alt"></i>
+                    </a>
+                  </li>
+                  <li class="quick-view">
+                    <a href="#">+ Quick View</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Shoes</div>
+                <a href="#">
+                  <h5>Guangzhou sweater</h5>
+                </a>
+                <div class="product-price">$13.00</div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/cloth/jaket_zara3-1.jpg" alt />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#">
+                      <i class="icon_bag_alt"></i>
+                    </a>
+                  </li>
+                  <li class="quick-view">
+                    <a href="#">+ Quick View</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Towel</div>
+                <a href="#">
+                  <h5>Pure Pineapple</h5>
+                </a>
+                <div class="product-price">$34.00</div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/cloth/jaket_zara4-1.jpg" alt />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#">
+                      <i class="icon_bag_alt"></i>
+                    </a>
+                  </li>
+                  <li class="quick-view">
+                    <a href="#">+ Quick View</a>
+                  </li>
+                  <li class="w-icon">
+                    <a href="#">
+                      <i class="fa fa-random"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Towel</div>
+                <a href="#">
+                  <h5>Converse Shoes</h5>
+                </a>
+                <div class="product-price">$34.00</div>
+              </div>
+            </div>
+          </carousel>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Women Banner Section End -->
+</template>
+
+<script>
+import carousel from "vue-owl-carousel"; // <- depedencies (javascript carousel untuk vue)
+
+export default {
+  name: "CarouselBanner",
+  components: {
+    carousel
+  }
+};
+</script>
+
+<style scoped>
+.product-item {
+  margin-right: 25px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderStore />
+    <CarouselStore />
+    <CarouselBanner />
+    <InstagramSection />
+    <PartnerSection />
+    <FooterStore />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+import HeaderStore from "@/components/HeaderStore.vue";
+import CarouselStore from "@/components/CarouselStore.vue";
+import CarouselBanner from "@/components/CarouselBanner.vue";
+import InstagramSection from "@/components/InstagramSection.vue";
+import PartnerSection from "@/components/PartnerSection.vue";
+import FooterStore from "@/components/FooterStore.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderStore,
+    CarouselStore,
+    CarouselBanner,
+    InstagramSection,
+    PartnerSection,
+    FooterStore
+  }
+};
+</script>
